954688c138de3f457aada4c050c162990a789d57
jkeny <jkeny_1985@hotmail.com>
2016-05-21 14:36:18 -0500

    login

---
+++ server/api/controllers/LoginController.js
@@ -0,0 +1,16 @@
+/**
+ * LoginController
+ *
+ * @description :: Server-side logic for managing logins
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	getInfo: function (req, res) {
+	Parking.query("CALL LOGIN_USER('" + req.query.name +  "','" + req.query.pass + ")", function(err, results) {
+		if (err) return res.serverError(err);
+		return res.json(results[0]);
+			});
+},
+
+};
